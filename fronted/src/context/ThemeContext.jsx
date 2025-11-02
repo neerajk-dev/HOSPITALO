29bf6a62e1ae6c9aa44bf8d3a58eead308d579cf
code-with-neeraj <neerajkr145518@gmail.com>
2025-11-02 20:05:16 +0530

    add dark them fea

--- fronted/src/context/ThemeContext.jsx
+++ fronted/src/context/ThemeContext.jsx
@@ -3,15 +3,13 @@ import React, { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ||
-      (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-  );
+  // Default is 'light'; use saved theme if it exists
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
   useEffect(() => {
     document.documentElement.classList.remove("light", "dark");
     document.documentElement.classList.add(theme);
-    // localStorage.setItem("theme", theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
